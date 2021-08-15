862497dfd126c0d183a79323fcd98e81e1aa6305
Dominick Lesperance <dominicklesperance@Citadel-MacBook.local>
2021-08-15 08:21:11 -0400

    Initial testing of updating commits on Github

---
+++ contracts/TodoList.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+    // taskCount belongs to entire contract,
+    // public adds functionality which makes it viewable
+    uint256 public taskCount = 0;
+}
