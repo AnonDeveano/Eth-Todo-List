9b7b096612004491e54553281c74b6d0d0b67036
DevGutta <dev_attug@protonmail.com>
2021-08-19 19:08:45 -0400

    List tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,4 +4,21 @@ contract TodoList {
     // taskCount belongs to entire contract,
     // public adds functionality which makes it viewable
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out dappuniversity.com");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
